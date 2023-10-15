8e70918c3e029dac38afb8eb3da585082b859c86
samusanc <samusanc@student.42madrid.com>
2023-10-15 18:05:09 +0200

    uploading the philo solving

--- philo/l_forks.c
+++ philo/l_forks.c
@@ -6,7 +6,7 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:15:27 by samusanc          #+#    #+#             */
-/*   Updated: 2023/10/12 19:20:48 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/10/15 17:49:55 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 static void	lock_fork(t_philo *philo, unsigned int id)
 {
 	pthread_mutex_lock(philo->m_philo_forks + id);
+	pthread_mutex_lock(philo->m_philo_printer);
+	if (philo->end == no)
+		printf("%u %u has taken a fork\n", get_actual_time(philo), id + 1);
+	pthread_mutex_unlock(philo->m_philo_printer);
 }
 
 static void	lock_next_fork(t_philo *philo, unsigned int id)
@@ -26,6 +30,10 @@ static void	lock_next_fork(t_philo *philo, unsigned int id)
 		pthread_mutex_lock(philo->m_philo_forks + fork);
 	else
 		pthread_mutex_lock(philo->m_philo_forks + 0);
+	pthread_mutex_lock(philo->m_philo_printer);
+	if (philo->end == no)
+		printf("%u %u has taken a fork\n", get_actual_time(philo), id + 1);
+	pthread_mutex_unlock(philo->m_philo_printer);
 }
 
 void	lock_forks(t_philo *philo, unsigned int id)
--- philo/main.c
+++ philo/main.c
@@ -6,10 +6,19 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 09:21:54 by samusanc          #+#    #+#             */
-/*   Updated: 2023/10/15 15:38:36 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/10/15 17:47:28 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <philo.h>
+
+int	one_philo(t_philo *philo)
+{
+	printf("%u 1 has taken a fork\n", get_actual_time(philo));
+	usleep(philo->time_2_die * 1000);
+	printf("%u 1 died\n", get_actual_time(philo));
+	free_all(philo);
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -27,6 +36,8 @@ int	main(int argc, char **argv)
 		philo.limit_meals = no;
 	if (philo_parsing(&philo, ++argv))
 		return (-1);
+	if (philo.number_of_philos == 1)
+		return (one_philo(&philo));
 	start_sim(&philo);
 	free_all(&philo);
 	return (0);
