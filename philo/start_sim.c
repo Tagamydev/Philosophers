43ddc4895013b654a869ff8d5a31c8ad1a2a2244
samusanc <samusanc@student.42madrid.com>
2023-10-20 13:25:36 +0200

    uploading the good norminette philo

--- philo/start_sim.c
+++ philo/start_sim.c
@@ -6,7 +6,7 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 15:39:57 by samusanc          #+#    #+#             */
-/*   Updated: 2023/10/15 16:04:08 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/10/20 13:25:03 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,18 @@ static int	start_sim_error(pthread_t *philos)
 	return (-1);
 }
 
+static int	start_fatness(t_philo *philo, pthread_t *death, pthread_t *philos)
+{
+	if (!philos)
+		return (start_sim_error(philos));
+	if (philo->limit_meals == yes)
+	{
+		if (pthread_create(death, NULL, &ft_death, (void *)philo))
+			return (start_sim_error(philos));
+	}
+	return (0);
+}
+
 int	start_sim(t_philo *philo)
 {
 	pthread_t		*philos;
@@ -26,16 +38,15 @@ int	start_sim(t_philo *philo)
 
 	i = 0;
 	philos = malloc(sizeof(pthread_t) * (philo->number_of_philos + 1));
-	if (!philos)
-		return (start_sim_error(philos));
-	if (pthread_create(&death, NULL, &ft_death, (void *)philo))
-		return (start_sim_error(philos));
+	if (start_fatness(philo, &death, philos) == -1)
+		return (-1);
 	while (i < philo->number_of_philos)
 	{
 		if (pthread_create(philos + i++, NULL, &routine, (void *)philo))
 			return (1);
 	}
-	pthread_join(death, NULL);
+	if (philo->limit_meals == yes)
+		pthread_join(death, NULL);
 	i = 0;
 	while (i < philo->number_of_philos)
 	{
